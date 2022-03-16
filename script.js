8b81776a4c85d15c796341b9e52596a498ead5fe
salma45667 <salmamalak555555@gmail.com>
2022-03-16 21:56:14 +0300

    cw 5 3

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ console.log(budget>30 && budget<80)
 
 budget= budget+30
 console.log(budget>30 && budget<80)
+
+let grade= prompt("What is your grade");
+if(grade>=90) {
+console.log=("🥳لقد حصلت على امتياز");
+}else if(grade>=80) { 
+console.log= ("🤩لقد حصلت على جيد جدا");
+}else if(grade>=70) {
+console.log =(" 🙂لقد حصلت على جيد");
+}else if(grade>=60) {
+ console.log=("😕لقد حصلت على مقبول") ;
+}else if(grade>=50) {
+ console.log=("☹️لقد حصلت على ضعيف") ;
+}else if(grade<50) {
+console.log= ("💔لقد رسبت");
+       }
